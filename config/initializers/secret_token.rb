9bce35b428e3c7977b2d24413fa5f7bb13712fef
Dmitri Severinov <soulwasher@ubuntu.(none)>
2013-08-14 00:15:37 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IstqbMaster::Application.config.secret_key_base = '6085777b635654d1d902cab8d4a77d11517c5e67d0d49b7404ee49ad8e2fa2eb275e9bf655cbcbeab098afb44724f817f87ecc029ad031dd1a43954f37c72b17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IstqbMaster::Application.config.session_store :cookie_store, key: '_istqb_master_session'
