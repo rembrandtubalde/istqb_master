0a8ab0645225b0fe99d2a310f310d339ce37759d
Dmitri Severinov <soulwasher@gmail.com>
2013-08-17 09:27:45 -0700

    Add user signup mechanism

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Dmitri Severinov"
+    email    "soulwasher@gmail.com"
+    password "evilhabob"
+    password_confirmation "evilhabob"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "UserPages" do
   subject { page }
   
+  # signup page tests
   describe "Sign up page" do
     before { visit signup_path }
     
@@ -13,7 +14,49 @@ describe "UserPages" do
 	it "should have correct title" do
 	  should have_title('ISTQB Master | Sign up')
 	end
-  
   end
+  
+  # signup process
+  describe "signup" do
+
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+	
+	## negative
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+	
+	## positive
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+        #fill_in "Certificate type", with: "CTFL"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+  
+  #profile page tests
+  describe "User profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+    
+    it "should contain user's name" do
+      should have_content(user.name)
+    end
+    
+    it "should have title containing user's name" do
+      should have_title(user.name)
+    end
+   end
   
 end
